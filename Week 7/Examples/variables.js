85e7bcf2007c38b6baa3798bda95ad581746453b
Kelveen Raj <kelveenrajv@gmail.com>
2022-11-19 23:41:09 +0800

    week 7 notes

---
+++ Week 7/Examples/variables.js
@@ -0,0 +1,40 @@
+// List of examples on how to write variables for different scenarios.
+
+// Example 1 (for old browsers)
+
+var priceApple = 2;
+var priceOrange = 3;
+
+var total = priceApple + priceOrange;
+console.log(total);
+
+// Example 2 (modern JavaScript)
+
+const priceFruit = 20;
+const priceVege = 30;
+
+let priceTotal = priceFruit + priceVege;
+console.log(priceTotal);
+
+// Different data types can be assigned to variables
+
+// String
+const name = "Alex";
+
+// Number
+const number = 10;
+
+// Boolean
+const answer = true;
+
+// Null
+const empty = null;
+
+// Undefined
+const value = undefined;
+
+// Object
+const value1 = { example: 2 };
+
+// Array
+const value2 = ["Jason", "Max", "Tom"];
